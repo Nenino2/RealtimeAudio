b44f5b87beccfb2156b64324939618493d34c28d
halbtale <halbtale@gmail.com>
2021-07-16 19:52:17 +0200

    Added

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
 const recordDom = document.getElementById('record_button');
-const remoteDom = document.getElementById('remote')
 
 async function record() {
   const mediaStream = await navigator.mediaDevices.getUserMedia({  video: false, audio: true });
   const mediaRecorder = new MediaRecorder(mediaStream, {mimeType:  'video/webm; codecs="opus"'});
   const chunks = []
-  mediaRecorder.ondataavailable = (event) => {chunks.push(event.data)}
-  mediaRecorder.start(100);
-
-  setTimeout(() => {
-    console.log(chunks);
-    mediaRecorder.stop();
-    const senderBlob = new Blob(chunks, {type: 'video/webm; codecs="opus"'});
-
-
-
-    const reader = new FileReader();
-    reader.readAsDataURL(senderBlob); 
-    reader.onloadend = async () => {
-        const base64data = reader.result;                
-        console.log(base64data); // data you'll save on gundb
-        const receiverBlob = await (await fetch(base64data)).blob();
-        console.log(receiverBlob)
-        const url = URL.createObjectURL(receiverBlob);
-        remoteDom.src = url;
-
+  const audio = new Audio();
+  const secondInterval = 3;
+  mediaRecorder.ondataavailable = (event) => {
+    if (event.data) {
+      chunks.push(event.data)
+      const senderBlob = new Blob(chunks, {type: 'video/webm; codecs="opus"'});
+      const reader = new FileReader();
+      reader.readAsDataURL(senderBlob); 
+      reader.onloadend = async () => {
+          const base64data = reader.result;                
+          const receiverBlob = await (await fetch(base64data)).blob();
+          const url = URL.createObjectURL(receiverBlob);
+          audio.src = url;
+          audio.play();
+          audio.currentTime = (chunks.length-1)*secondInterval
+          console.log(audio);
+      }
     }
-  }, 5000);
+  }
+  mediaRecorder.start(secondInterval*1000);
 }
 
 
